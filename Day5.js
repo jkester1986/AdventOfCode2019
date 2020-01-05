cb09ed5c5b79a2414145d59335e8bd4deb05df87
Julia Kester <kester@oswego.edu>
2020-01-05 18:53:46 -0500

    refactoring & add mode reading

--- Day5.js
+++ Day5.js
@@ -1,4 +1,6 @@
 fs = require('fs');
+intcode = require('./intcode_computer.js');
+
 fs.readFile('Day5.txt', 'utf8', function (err, data) {
 
 	if (err) {
@@ -7,49 +9,6 @@ fs.readFile('Day5.txt', 'utf8', function (err, data) {
 
 	let register = data.split(',').map(Number);
 
-	function getOutput() {
-		let myReg = [...register];
+	console.log("P1:", intcode.compute(register[1], register[2], register));
 
-		let position = 0,
-			opCode = myReg[0];
-
-		while (opCode != 99) {
-			let storeLoc;
-
-			switch (opCode) {
-				// sum values from positions noted in the inputs
-				case 1:
-					let sum = myReg[myReg[position + 1]] + myReg[myReg[position + 2]];
-					storeLoc = myReg[position + 3];
-					myReg[storeLoc] = sum;
-					position += 4;
-					break;
-				// multiply values from positions noted in the inputs
-				case 2:
-					let multiple = myReg[myReg[position + 1]] * myReg[myReg[position + 2]];
-					storeLoc = myReg[position + 3];
-					myReg[storeLoc] = multiple;
-					position += 4;
-					break;
-				// set value of position noted in input. Value = position
-				case 3:
-					let setVal = myReg[position +1];
-					myReg[setVal] = setVal;
-					position += 2;
-					break;
-				//log value at position deteremed by input
-				case 4:
-					process.stdout.write(myReg[myReg[position + 1]]);
-					position += 2;
-					break;
-				default:
-					console.error("Something went wrong, myReg opcode is", opCode, "position is", position);
-					process.exit();
-					break;
-			}
-			opCode = myReg[position];
-		}
-
-		return myReg[0];
-	}
 });
--- intcode_computer.js
+++ intcode_computer.js
@@ -3,23 +3,26 @@ module.exports = {
 		let myReg = [...register];
 
 		let position = 0,
-			opCode = myReg[0];
+			instruction = myReg[0];
 			myReg[1] = noun;
 			myReg[2] = verb;
 
+		let { opCode, modeA, modeB, modeC } = getModes(instruction);
+
 		while(opCode != 99) {
 			let storeLoc;
+			let val1 = myReg[getValueFromMode(modeA, position + 1, register)],
+				val2 = myReg[getValueFromMode(modeB, position + 2, register)],
+				val3 = getValueFromMode(modeC, position + 3, register);
 
 			switch(opCode) {
 				case 1:
-					let sum = myReg[myReg[position + 1]] + myReg[myReg[position + 2]];
-					storeLoc = myReg[position + 3];
-					myReg[storeLoc] = sum;
+					let sum = val1 + val2;
+					myReg[val3] = sum;
 					break;
 				case 2:
-					let multiple = myReg[myReg[position + 1]] * myReg[myReg[position + 2]];
-					storeLoc = myReg[position + 3];
-					myReg[storeLoc] = multiple;
+					let multiple = val1 * val2;
+					myReg[val3] = multiple;
 					break;
 				default:
 					console.error("Something went wrong, myReg opcode is", opCode, "position is", position);
@@ -31,6 +34,35 @@ module.exports = {
 		}
 
 		return myReg[0];
+	}
+}
+
+function getModes(instruction) {
+	let opCode, modeA, modeB, modeC;
+
+	stringIns = instruction.toString();
+	if (stringIns.length === 1) {
+		opCode = instruction;
+		modeA = 0;
+		modeB = 0;
+		modeC = 0;
+	}
+	else {
+		opCode = parseInt(stringIns[stringIns.length - 1] + stringIns[stringIns.length - 2]);
+		modeC = stringIns.length >= 3 ? parseInt(stringIns[stringIns.length - 3]) : 0;
+		modeB = stringIns.length >= 4 ? parseInt(stringIns[stringIns.length - 4]) : 0;
+		modeA = modeB = stringIns.length === 5 ? parseInt(stringIns[0]) : 0;
+	}
+
+	return { opCode, modeA, modeB, modeC }
+}
+
+function getValueFromMode(mode, position, register) {
+	switch(mode) {
+		case 0:
+			return register[position];
+		case 1:
+			return position;
 	}
 }
 
